d0da78355c868a601a499a0255bc70d76f6ceff0
Barney <thtinh@Barney.local>
2010-08-27 02:20:41 +0700

    add slideshow frontpage, videoclip, updated database

--- tmp/com_kero/site/models/kero.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die();
-jimport( 'joomla.application.component.model' );
-
-class KeroModelKero extends JModel {
-   
-}
-?>
